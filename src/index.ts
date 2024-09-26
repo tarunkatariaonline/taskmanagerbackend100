4142550c3c75afd19526e48047783c471f8f7d1b
tarunkatariaonline <tarunsoftsol@gmail.com>
2024-09-26 16:58:47 +0530

    first

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { initServer } from "./graphql/index";
 import { Request,Response } from "express";
 async function init(){
     const app = await initServer()
+    const PORT = process.env.PORT || 8000
     app.get('/',(req:Request,res:Response)=>{
         res.send('Hello World Server is working')
     })
-    app.listen(8000,()=>{
+    app.listen(PORT,()=>{
         console.log('Server is running on port 8000')
     })
 }
